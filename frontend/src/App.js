04be8f32468ff17ee2157cd6df5a2a80ceaa6328
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-15 23:09:06 +0300

    feat: Add navbar and routing to ads page in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.scss';
 import Navbar from './components/Navbar/Navbar.jsx';
-
+import Ads from './pages/Ads/Ads.jsx';
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar
+        links={['Ads', 'Contact us', 'Log In', 'Sign Up']}
+        />
+        <Routes>
+          <Route path='/ads' element={<Ads/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
