4696f87877b9a4f200e5690ed08360d7612f2376
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-15 23:20:14 +0300

    refactor: App component to include home page route
    I moved the navbar from the App component to the Homepage (Home.jsx)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
 import './App.scss';
-import Navbar from './components/Navbar/Navbar.jsx';
+import Home from './pages/Home/Home.jsx';
 import Ads from './pages/Ads/Ads.jsx';
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar
-        links={['Ads', 'Contact us', 'Log In', 'Sign Up']}
-        />
         <Routes>
+          <Route path='/' element={<Home/>}/>
           <Route path='/ads' element={<Ads/>}/>
         </Routes>
       </BrowserRouter>
