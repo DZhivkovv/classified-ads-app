53410fe0617a41faac2b7c3323e0c73bc3b6ff46
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-16 21:03:30 +0300

    feat: Add hero section in homepage and style it

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import HeroTile from "../../components/HeroTile/HeroTile";
 import './Home.scss'
 
 export default function Home(){
@@ -8,6 +9,34 @@ export default function Home(){
             <Navbar
                 links={['Ads', 'Contact us', 'Log In', 'Sign Up']}
             />
+
+            <div className="home--hero-section">
+                <h1>What are you looking for today?</h1>
+
+                <div className="hero--grid">
+                    <HeroTile
+                    tileNum = {1}
+                    />
+                    <HeroTile
+                    tileNum = {2}
+                    />
+                    <HeroTile
+                    tileNum = {3}
+                    />
+                    <HeroTile
+                    tileNum = {4}
+                    />
+                    <HeroTile
+                    tileNum = {5}
+                    />
+                    <HeroTile
+                    tileNum = {6}
+                    />
+                    <HeroTile
+                    tileNum = {7}
+                    />
+                </div>
+           </div>
         </div>
     )
 }
