dd2da393b0ad6a8797ed76419f9343dbb7fe82ab
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-17 17:40:27 +0300

    feat: Add routes for Signup, Login pages in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import Home from './pages/Home/Home.jsx';
 import Ads from './pages/Ads/Ads.jsx';
+import SignUp from './pages/SignUp/SignUp';
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/ads' element={<Ads/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/login' element={<Ads/>}/>
         </Routes>
       </BrowserRouter>
     </div>
