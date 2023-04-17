68a4603b63f2eb24ec073667c1c2bdee92ec4459
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-17 17:43:54 +0300

    feat: Create the signup page

---
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+import './SignUp.scss'
+
+export default function SignUp(){
+    return(
+        <div className="signUp--container">
+            <div className="form-box">
+                <h1>Create account</h1>
+                <form>
+                    <div className="input-group">
+                        <div className="input-field">
+                            <i class="fa-solid fa-user"></i>
+                            <input type="email" name="email" placeholder="Email"></input>
+                        </div>
+
+                        <div className="input-field">
+                            <i class="fa-solid fa-envelope"></i>
+                            <input type="text" name="username" placeholder="Username"></input>
+                        </div>
+
+                        <div className="input-field">
+                            <i class="fa-solid fa-lock"></i>
+                            <input type="password" name="password" placeholder="Password"></input>
+                        </div>
+                        <a href="#">Forgotten password?</a>
+                    </div>
+                    <button>Sign up</button>
+                </form>
+                <p>Already have an account? <Link to='/login'>Log in</Link></p>
+
+            </div>
+        </div>
+    )
+}
