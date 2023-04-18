c170c5e3430610d7bbe1ac41121e128ae2313f05
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-18 12:52:31 +0300

    feat: Add basic form validation for the signup form

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -11,17 +11,18 @@ export default function SignUp(){
                     <div className="input-group">
                         <div className="input-field">
                             <i class="fa-solid fa-user"></i>
-                            <input type="email" name="email" placeholder="Email"></input>
+                            <input type="email" name="email" placeholder="Email" required></input>
                         </div>
 
                         <div className="input-field">
                             <i class="fa-solid fa-envelope"></i>
-                            <input type="text" name="username" placeholder="Username"></input>
+                            <input type="text" name="username" placeholder="Username" minLength={4} required></input>
                         </div>
 
                         <div className="input-field">
                             <i class="fa-solid fa-lock"></i>
-                            <input type="password" name="password" placeholder="Password"></input>
+                            <input type="password" name="password" placeholder="Password" pattern="^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" required></input>
+                            <p className="validation-msg">Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number.</p>
                         </div>
                     </div>
                     <button>Sign up</button>
