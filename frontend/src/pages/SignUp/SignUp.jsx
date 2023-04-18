25a395d085187b75fc9f768643c3d5d1bb598293
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-18 21:16:19 +0300

    refactor: Add handleChange and handleSubmit to inputs in SignUp.jsx

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -4,9 +4,9 @@ import './SignUp.scss'
 
 export default function SignUp(){
     const [user, setUser] = React.useState({
-        "email":"",
-        "username":"",
-        "password":"",
+        email:"",
+        username:"",
+        password:"",
     })
 
     function handleChange(e){
@@ -37,21 +37,21 @@ export default function SignUp(){
         <div className="signUp--container">
             <div className="form-box">
                 <h1>Create account</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="input-group">
                         <div className="input-field">
-                            <i class="fa-solid fa-user"></i>
-                            <input type="email" name="email" placeholder="Email" required></input>
+                            <i className="fa-solid fa-envelope"></i>
+                            <input type="email" name="email" placeholder="Email" onChange={handleChange} required></input>
                         </div>
 
                         <div className="input-field">
-                            <i class="fa-solid fa-envelope"></i>
-                            <input type="text" name="username" placeholder="Username" minLength={4} required></input>
+                            <i className="fa-solid fa-user"></i>
+                            <input type="text" name="username" placeholder="Username" minLength={4} onChange={handleChange} required></input>
                         </div>
 
                         <div className="input-field">
-                            <i class="fa-solid fa-lock"></i>
-                            <input type="password" name="password" placeholder="Password" pattern="^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" required></input>
+                            <i className="fa-solid fa-lock"></i>
+                            <input type="password" name="password" placeholder="Password" pattern="^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" onChange={handleChange} required></input>
                             <p className="validation-msg">Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number.</p>
                         </div>
                     </div>
