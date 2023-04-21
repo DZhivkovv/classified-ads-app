50454af8c5a08b7e3a3ca3132ab574caa3567fd0
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-21 14:12:49 +0300

    feat: Add menu and hide login/signup buttons for logged usersin Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -28,6 +28,41 @@ export default function Navbar(props){
                 <img src={logo} alt="A temporary logo until i make one" className="nav--elelment logo"/>
             </Link>
                 {allLinks}
+                {/* This part of the navigation will only be visible if the user is not logged into their account. */}                               
+                <li className={props.successfulLogin? "hidden" : "nav--element login"}>
+                    <Link to='/login'>
+                        Login
+                    </Link>
+                </li>
+                <li className = {props.successfulLogin? "hidden" : "nav--element signup"}>
+                    <Link to='/signup'>
+                        Sign up
+                    </Link>
+                </li>
+               
+                {/* This part of the navigation will only be visible if the user is logged into their account. */}                
+                <li className= {props.successfulLogin?"nav--element loggedUser" : "hidden" }>
+                    <div className="loggedUser--details">
+                        <p className="loggedUser--username">{props.username}</p>
+                        <img src={userPhoto} className="loggedUser--profile" alt="Profile pic of the user" onClick={handleClick}></img>
+
+                        {/* This menu will be shown and hidden when the profile picture is clicked. */}
+                        <div className="loggedUser--menu hidden" ref={elementRef}>
+                            <div className="menu-item menu--profile">
+                                <i className="fa-solid fa-user"></i>
+                                <Link to='/your-profile'>Your profile</Link>
+                            </div>
+                            <div className="menu-item menu--inventory">
+                                <i className="fa-solid fa-boxes-stacked"></i>
+                                <Link to='/your-ads'>Your ads</Link>
+                            </div>
+                            <div className="menu-item menu--logout">
+                                <i className="fa-solid fa-right-from-bracket"></i>
+                                <Link to='/logout'>Logout</Link>
+                            </div>
+                        </div>
+                    </div> 
+                </li>
             </ul>
         </nav>
     )
