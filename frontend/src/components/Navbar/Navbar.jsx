5ae5636956ce9d5df8c361ac26f0e00b2afc5132
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-21 14:09:26 +0300

    feat: Add reference to user profile menu in Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import './Navbar.scss'
 import logo from '../../assets/—Pngtree—corporate image logo free logo_1026060.png'
 
 export default function Navbar(props){
+    //Create reference to the profile menu in navigation that will show and hide everytime the profile image is clicked 
+    const elementRef = useRef(null);
+
+    function handleClick(){
+        elementRef.current.classList.toggle('hidden');
+    }
+
     const allLinks = props.links.map(
         link => 
         <li key={link} className = {"nav--element " + link.replace(/\s/g, '').toLowerCase()}>
