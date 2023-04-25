782d2d869a613ad8d914966cd34bd23a5ad12f2c
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-26 00:23:42 +0300

    refactor: Change API url from '/login' to '/api/login' in Login.jsx

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -21,7 +21,7 @@ export default function Login(){
     async function handleSubmit(e){
         e.preventDefault();
     
-        const response = await fetch('http://localhost:3001/login', {
+        await fetch('http://localhost:3001/api/login', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
