044b9a62478551ea711bdd89caa3a9b3d8362190
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-26 00:17:36 +0300

    refactor: Change API endpoint URL from '/signup' to '/api/signup'

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -22,7 +22,7 @@ export default function SignUp(){
     const handleSubmit = async (event) => {
         event.preventDefault();
     
-        const response = await fetch('http://localhost:3001/signup', {
+        const response = await fetch('http://localhost:3001/api/register', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
