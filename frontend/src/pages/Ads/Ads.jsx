20aef21a0f28546409cbe852d1f75aad236cb867
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-28 21:06:24 +0300

    feat: Add navigation and a button for adding a new ad in Ads page

--- frontend/src/pages/Ads/Ads.jsx
+++ frontend/src/pages/Ads/Ads.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
 import './Ads.scss'
 
 export default function Ads(){
     return(
         <div className="ads--container">
-            <h1>Ads</h1>       
+            <Navbar
+                links={['Home', 'Contact us']}
+            />
+            <section className="ads--main-section">
+                <h1>Ads</h1>       
+                <Link to='/addad' className="add-an-ad-btn">+</Link>
+            </section>
         </div>
     )
 }
