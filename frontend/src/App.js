15372f6db96a259fd1a363ac514c847d5a25ed62
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-04-28 21:00:34 +0300

    feat: Add route for the page 'Add an ad'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import Home from './pages/Home/Home.jsx';
 import Ads from './pages/Ads/Ads.jsx';
+import AddClassifiedAd from './pages/AddAd/AddAd';
 import SignUp from './pages/SignUp/SignUp';
 import Login from './pages/Login/Login';
 import UserProfile from './pages/UserProfile/UserProfile'
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/ads' element={<Ads/>}/>
+          <Route path='/addad' element={<AddClassifiedAd/>}/>
           <Route path='/signup' element={<SignUp/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path="/users/:username" element={<UserProfile/>} />
