7c00e2a2c4400923bfccb1d5aa2c1449ebfe24ac
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-05-02 13:58:29 +0300

    feat: Implement image upload with multer in ad creation endpoin

--- backend/routes/adRoutes.js
+++ backend/routes/adRoutes.js
@@ -4,6 +4,27 @@ import { saveAdvertisement, getAllAds } from '../controllers/adController.js';
 
 const adRouter = express.Router();
 
-adRouter.post('/advertisements', saveAdvertisement)
+const storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, '../frontend/public/images');
+    },
+
+    filename: function(req, file, cb){
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+});
+
+const fileFilter = (req,file, cb) => {
+    const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png']
+    if(allowedFileTypes.includes(file.mimetype)){
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+const upload = multer({storage, fileFilter})
+
+adRouter.post('/advertisements', upload.single('images'), saveAdvertisement)
 
 export default adRouter
