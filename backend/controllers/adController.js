e1968e0c383a2f1143cbeb091ed468ce2b0d73b5
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-05-03 19:15:38 +0300

    feat: Add controller function for retrieving data about a single ad
    This commit adds a new function, getSingleAd, that retrieves a single ad
    by ID and returns its details as a JSON object in the response. The
    function uses the findById method of the Ad model to fetch the ad data
    from the database, and sends the data to the client with a 200 status
    code. If there is an error, the function logs the error to the console.

--- backend/controllers/adController.js
+++ backend/controllers/adController.js
@@ -33,3 +33,22 @@ export const getAllAds = async (request, response, next) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 }
+
+export const getSingleAd = async ( request, response, next) => {
+    try{
+        const id = request.params.id;
+        const ad = await Ad.findById(id);
+        response.send({
+            status:200,
+            id:ad._id,
+            title:ad.title,
+            description:ad.description,
+            price:ad.price,
+            date:ad.date,
+            username: ad.username,
+            images: ad.images,
+        })
+    } catch ( error ){
+        console.error(error)
+    }
+}
