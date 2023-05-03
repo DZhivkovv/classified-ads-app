0d286c8b62d032d8b59dcad5b26baaab9b44bb7b
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-05-03 13:03:31 +0300

    refactor: Improve Ad component structure and update class names

--- frontend/src/components/Ad/Ad.jsx
+++ frontend/src/components/Ad/Ad.jsx
@@ -2,12 +2,23 @@ import './Ad.scss'
 export default function Ad(props){
     return(
         <div className='ad'>
-            <img src={`images/${props.images}`} className='ad--image' alt={`${props.title}`}/>
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
-            <p>{props.price}</p>
-            <p>{props.date}</p>
-            <p>Uploaded by {props.username}</p>
+            <div className='ad-image'>
+                <img src={`images/${props.images}`} alt={`${props.title}`}/>
+            </div>
+            <div className='ad-main-text'>
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
+            </div>
+            <div className='ad-details'>
+                <div className='ad-details-text'>
+                <p>{props.price} leva</p>
+                <p>by {props.username}</p>
+                <p>{props.date.slice(0,10)}</p>
+                </div>
+                <div className='button-container'>
+                    <button>See details</button>
+                </div>
+            </div>
         </div>
     )
 }
