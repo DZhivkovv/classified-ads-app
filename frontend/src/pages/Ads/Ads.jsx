f1d4dece26e9a5c7398a4771336754b56221dd78
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-05-06 17:22:15 +0300

    feat: Display only the first image of the image array in Ad component

--- frontend/src/pages/Ads/Ads.jsx
+++ frontend/src/pages/Ads/Ads.jsx
@@ -23,7 +23,7 @@ export default function Ads(){
             <section className="ads--main-section">
                 {
                     ads?
-                    ads.map(ad => <Ad key = {ad._id} id = {ad._id} title = {ad.title} description = {ad.description} price = {ad.price} username = {ad.username} date={ad.date} images = {ad.images}/>)
+                    ads.map(ad => <Ad key = {ad._id} id = {ad._id} title = {ad.title} description = {ad.description} price = {ad.price} username = {ad.username} date={ad.date} images = {ad.images[0]}/>)
                     :
                     null
                 }                
