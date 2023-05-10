c04f0ff13eec118cd8f301f28bb0afae895aae96
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-05-10 21:01:00 +0300

    feat: Add search function and pagination in Ads.jsx

--- frontend/src/pages/Ads/Ads.jsx
+++ frontend/src/pages/Ads/Ads.jsx
@@ -5,39 +5,74 @@ import Ad from "../../components/Ad/Ad.jsx";
 import './Ads.scss'
 
 export default function Ads() {
-    const [ads, setAds] = useState(null); // State for storing all ads
-    const [numberOfPages, setNumberOfPages] = useState(0);// State for storing the total number of pages 
-    const [activePage, setActivePage] = useState(0);// State fpr storing the page number the user is on
+    const [ads, setAds] = useState(null); // Stores all ads in the database
+
+
+    //States and variables for searching and filtering ads
+    const [searchedAds, setSearchedAds] = useState(""); // Stores the ads the user is specifically searching for (filtered ads)
+    const [searchQuery, setSearchQuery] = useState(""); // Stores the search query
+    const handleSearch = (event) => {
+        event.preventDefault();
+        setSearchQuery(event.target.value);
+        setFilteredPageNumber(0); // Reset pageNumber to 0 for new search
+
+    };
+    const [category, setCategory] = useState(""); // Stores the category for filtering ads by category
+    const [selectedCategory, setSelectedCategory] = useState(""); // Stores the selected category
+
+    const categories = ['Real Estate', 'Vehicles', 'Electronics', 'Home and Garden', 'Services', 'Jobs', 'Clothing and Shoes', 'Pets']; //The categories an ad can be.
+
+    const handleCategoryChange = (event) => {
+        const category = event.target.value;
+        setCategory(category);
+        setSelectedCategory(category);
+      };
+        
+
+    //Pagination states, variables and functions
+    const [numberOfPages, setNumberOfPages] = useState(0);// Stores the total number of pages 
     const [pageNumber, setPageNumber] = useState(0);
-    const [searchedAds, setSearchedAds] = useState(null); // State for storing searched ads
-    const [searchQuery, setSearchQuery] = useState(""); // State for storing search query
-
-
-    const pages = new Array(numberOfPages).fill(null).map((v, i) => i); // Creates an array of page numbers from 0 to numberOfPages-1
-    // Each element represents a page number
+    const [activePage, setActivePage] = useState(0);// Stores the page number the user is on
+    const pages = new Array(numberOfPages).fill(null).map((v, i) => i); // Each element represents a page number for all ads
     
     const gotoPrevious = () => {
-      setPageNumber(Math.max(0, pageNumber - 1));
-      // Set the pageNumber state to the maximum value between 0 and pageNumber - 1
-      // Ensures that the page number doesn't go below 0
-    
-      setActivePage(Math.max(0, pageNumber - 1));
-      // Set the activePage state to the same value as pageNumber
-      // Updates the active button to reflect the updated page number
-    };
-    
-    const gotoNext = () => {
-      setPageNumber(Math.min(numberOfPages - 1, pageNumber + 1));
-      // Set the pageNumber state to the minimum value between numberOfPages - 1 and pageNumber + 1
-      // Ensures that the page number doesn't exceed the total number of pages
-    
-      setActivePage(Math.min(numberOfPages - 1, pageNumber + 1));
-      // Set the activePage state to the same value as pageNumber
-      // Updates the active button to reflect the updated page number
-    };
+        setPageNumber(Math.max(0, pageNumber - 1)); //Set the pageNumber state to the maximum value between 0 and pageNumber - 1. Ensures that the page number doesn't go below 0 
+        setActivePage(Math.max(0, pageNumber - 1)); // Set the activePage state to the same value as pageNumber. Updates the active button to reflect the updated page number
+    }; 
 
+    const gotoNext = () => {
+        setPageNumber(Math.min(numberOfPages - 1, pageNumber + 1));
+        // Set the pageNumber state to the minimum value between numberOfPages - 1 and pageNumber + 1
+        // Ensures that the page number doesn't exceed the total number of pages
+      
+        setActivePage(Math.min(numberOfPages - 1, pageNumber + 1));
+        // Set the activePage state to the same value as pageNumber
+        // Updates the active button to reflect the updated page number
+    };
+  
+    //Pagination for filtered ads
+    const [filteredNumberOfPages, setFilteredNumberOfPages] = useState(0); //Stores the total number of pages for filtered ads
+    const [filteredPageNumber, setFilteredPageNumber] = useState(0);
+    const [filteredActivePage, setFilteredActivePage] = useState(0);// State for storing the page number the user is on
+    const filteredPages = new Array(filteredNumberOfPages).fill(null).map((v, i) => i); // // Each element represents a page number for filtered ads
+
+    const gotoFilteredPrevious = () => {
+        setFilteredPageNumber(Math.max(0, filteredPageNumber - 1));
+        // Set the pageNumber state to the maximum value between 0 and pageNumber - 1. Ensures that the page number doesn't go below 0 
+        setFilteredActivePage(Math.max(0, filteredPageNumber - 1));
+        // Set the activePage state to the same value as pageNumber. Updates the active button to reflect the updated page number
+    };
+  
+    const gotoFilteredNext = () => {
+        setFilteredPageNumber(Math.min(filteredNumberOfPages- 1, filteredPageNumber + 1));
+        // Set the pageNumber state to the minimum value between numberOfPages - 1 and pageNumber + 1. Ensures that the page number doesn't exceed the total number of pages
+        setFilteredActivePage(Math.min(filteredNumberOfPages - 1, filteredPageNumber + 1));
+        // Set the activePage state to the same value as pageNumber. Updates the active button to reflect the updated page number
+    };
+      
+
+    // Fetches all ads when the component mounts
     useEffect(() => {
-        // Fetch all ads when the component mounts
         fetch(`http://localhost:3001/getAllAds?page=${pageNumber}`)
             .then(response => response.json())
             .then(data => {
@@ -46,17 +81,24 @@ export default function Ads() {
             });
     }, [pageNumber]);
 
-    useEffect(() => {
-        // Fetch searched ads when searchQuery changes
-        fetch(`http://localhost:3001/searchAds?search=${encodeURIComponent(searchQuery)}`)
-            .then(response => response.json())
-            .then(data => setSearchedAds(data));
-    }, [searchQuery]);
 
-    const handleSearch = (event) => {
-        event.preventDefault();
-        setSearchQuery(event.target.value);
-    }
+
+    // Fetches searched ads according to search query, category or both (when the search query, category filter or filteredPageNumber changes)
+    useEffect(() => {
+        let url = `http://localhost:3001/searchAds?search=${encodeURIComponent(searchQuery)}`;
+        if (category) {
+          url += `&category=${encodeURIComponent(category)}`;
+        }
+        url += `&page=${filteredPageNumber}`;
+      
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            setSearchedAds(data.ads);
+            setFilteredNumberOfPages(data.totalPages)
+        });
+    }, [searchQuery, category, filteredPageNumber]);
+
 
     return (
         <div className="ads--container">
@@ -65,49 +107,143 @@ export default function Ads() {
             />
             <section className="main-section">
                 <div className="ads-search">
-                    <form onSubmit={handleSearch}>
-                        <input type="text" className="search-title" value={searchQuery} onChange={handleSearch} placeholder="Search" />
-                        <button>Search</button>
-                    </form>
-                </div>
-                <div className="all-ads">
-                    {/* 
-                    Render ads based on search results.
-                    If no ads match the searched query, just display all ads in the database. Otherwise, display only the ads that match the query.
-                    */}
-                    {
-                        searchedAds && searchedAds.length === 0 ? (
-                            ads ? (
-                                ads.map(ad => <Ad key={ad._id} id={ad._id} title={ad.title} description={ad.description} price={ad.price} username={ad.username} date={ad.date} images={ad.images[0]} />)
-                            ) : null
-                        ) : (
-                            searchedAds && searchedAds.map(ad => <Ad key={ad._id} id={ad._id} title={ad.title} description={ad.description} price={ad.price} username={ad.username} date={ad.date} images={ad.images[0]} />)
-                        )
-                    }
-                </div>
- 
-                <div className="pages-controller">
-                    {/* Render the button to navigate to the previous page */}
-                    <button onClick={gotoPrevious}><i class="fa-solid fa-up-long"></i></button>
-                    {pages.map((pageIndex) => (
-                        <button
-                            key={pageIndex}
-                            onClick={() => {
-                            setPageNumber(pageIndex);
-                            setActivePage(pageIndex);
-                            }}
-                            /* Apply the "active" class if the button's page index matches the activePage state */
-                            className={activePage === pageIndex ? "active" : "pgsbtn"}
+                <form onSubmit={handleSearch}>
+
+                {/*Search bar for searching ads */}
+                <input
+                type="text"
+                className="search-title"
+                onChange={handleSearch}
+                placeholder="Search"
+                />
+
+
+                {/*Filter ads by categories */}
+                <label className="category-option">
+                <input
+                    type="radio"
+                    name="category"
+                    value=""
+                    checked={selectedCategory === ""}
+                    onChange={handleCategoryChange}
+                />
+                All Categories
+                </label>
+                
+                {categories.map((category, index) => (
+                <label key={index} className="category-option">
+                    <input
+                    type="radio"
+                    name="category"
+                    value={category}
+                    checked={category === selectedCategory}
+                    onChange={handleCategoryChange}
+                    />
+                    {category}
+                </label>
+                ))}
+
+          </form>
+        </div>
+
+        <div className="all-ads">
+            {searchedAds && searchedAds.length === 0 ? (
+                ads ? 
+                (
+                ads.map((ad) => (
+                    <Ad
+                        key={ad._id}
+                        id={ad._id}
+                        title={ad.title}
+                        description={ad.description}
+                        price={ad.price}
+                        username={ad.username}
+                        category={ad.category}
+                        date={ad.date}
+                        images={ad.images[0]}
+                        freeShipping={ad.isFreeShipping}
+                    />
+                    ))
+                ) : null
+            ) 
+            : 
+            (
+            searchedAds &&
+            searchedAds.map((ad) => (
+                    <Ad
+                        key={ad._id}
+                        id={ad._id}
+                        title={ad.title}
+                        description={ad.description}
+                        price={ad.price}
+                        username={ad.username}
+                        category={ad.category}
+                        date={ad.date}
+                        images={ad.images[0]}
+                        freeShipping={ad.isFreeShipping}
+                    />
+                ))
+            )}
+        </div>
+
+        {/*Pagination for all ads*/}
+        {searchedAds && searchedAds.length === 0 && (
+            <div className="pages-controller">
+                {/* Render the button to navigate to the previous page */}
+                <button onClick={gotoPrevious}><i class="fa-solid fa-up-long"></i></button>
+                
+                {pages.map((pageIndex) => (
+                    <button
+                        key={pageIndex}
+                        onClick={() => {
+                        setPageNumber(pageIndex);
+                        setActivePage(pageIndex);
+                        }}
+                        /* Apply the "active" class if the button's page index matches the activePage state */
+                        className={activePage === pageIndex ? "active" : "pgsbtn"}
                         >
+                            
                         {/* Display the page number on the button */}
                         {pageIndex + 1}
-                        </button>
-                    ))}
-                  {/* Render the button to navigate to the next page */}
-                  <button onClick={gotoNext}><i class="fa-solid fa-down-long"></i></button>
-                </div>
-                <Link to='/addad' className="add-an-ad-btn">+</Link>
-            </section>
-        </div>
+                    </button>
+                ))}
+                        
+                {/* Render the button to navigate to the next page */}
+                <button onClick={gotoNext}><i class="fa-solid fa-down-long"></i></button>
+            </div>
+        )}
+
+        {/*Pagination for filtered ads */}
+        {searchedAds && searchedAds.length > 0 && (
+            <div className="pages-controller">
+                {/* Render the button to navigate to the previous page */}
+                <button onClick={gotoFilteredPrevious}><i class="fa-solid fa-up-long"></i></button>
+                    
+                {/*Page buttons*/}
+                {filteredPages.map((pageIndex) => (
+                    <button
+                        key={pageIndex}
+                        onClick={() => {
+                        setFilteredPageNumber(pageIndex);
+                        setFilteredActivePage(pageIndex);
+                        }}
+                        /* Apply the "active" class if the button's page index matches the activePage state */
+                        className={filteredActivePage === pageIndex ? "active" : "pgsbtn"}>
+
+                        {/* Display the page number on the button */}
+                        {pageIndex + 1}
+                
+                    </button>
+                ))}
+
+                {/* Render the button to navigate to the next page */}
+                    <button onClick={gotoFilteredNext}><i class="fa-solid fa-down-long"></i></button>
+            </div>
+            )}
+
+            {/*Button for publishing an ad. It redirects to the ad publishing form.*/} 
+            <Link to='/addad' className="add-an-ad-btn">+</Link>
+        </section>
+    </div>
     );
 }
