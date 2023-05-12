9c8048d0aa84b8c89f102226322cebaf119d2f1f
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-05-12 11:56:17 +0300

    feat(HeroTile.jsx): Add the images props passed by Home.jsx in the tiles

--- frontend/src/components/HeroTile/HeroTile.jsx
+++ frontend/src/components/HeroTile/HeroTile.jsx
@@ -3,8 +3,8 @@ import './HeroTile.scss'
 
 export default function HeroTile(props){
     return(
-        <div className={'tile--container ' + 'tile' + props.tileNum}>
-            Random section 
+        <div className={'tile--container ' + 'tile' + props.tileNum} style={tileStyle}>
+            <img src={props.imgSrc} alt=''></img>
         </div>
     )
 }
