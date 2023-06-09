918c505bb2f7085c0061a7149552d9af81413ced
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-06-09 15:09:23 +0300

    feat: Specify routes to UnderConstruction page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import AddClassifiedAd from './pages/AddAd/AddAd';
 import SignUp from './pages/SignUp/SignUp';
 import Login from './pages/Login/Login';
 import UserProfile from './pages/UserProfile/UserProfile'
+import UnderConstruction from './pages/UnderConstructionPage/UnderConstruction';
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
 function App() {
   return (
@@ -13,10 +14,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/contactus' element={<UnderConstruction/>}/>
           <Route path='/ads' element={<Ads/>}/>
           <Route path='/ads/:id' element={<Ad/>}/>
           <Route path='/addad' element={<AddClassifiedAd/>}/>
           <Route path='/register' element={<SignUp/>}/>
+          <Route path="/message/:id" element={<UnderConstruction/>} />
           <Route path='/login' element={<Login/>}/>
           <Route path="/users/:id" element={<UserProfile/>} />
         </Routes>
