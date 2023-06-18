a1c18fde9718281655e993af7aa1e43980afb081
Dobromir Zhivkov <dobromirzhivkovv@gmail.com>
2023-06-18 15:18:48 +0300

    feat: Display loader during signup process

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import { Link, useNavigate } from 'react-router-dom'
+import { Oval } from  'react-loader-spinner'
 import './SignUp.scss'
 
 export default function SignUp(){
@@ -8,9 +9,11 @@ export default function SignUp(){
         username:"",
         password:"",
         location: "",
-    })
+    });
 
-    const navigate = useNavigate()
+    const [isLoading, setIsLoading] = useState(false);
+
+    const navigate = useNavigate();
 
 
     useEffect(() => {
@@ -35,7 +38,8 @@ export default function SignUp(){
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-    
+        setIsLoading(true);
+
         const response = await fetch('https://classified-ads-app.onrender.com/api/register', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
@@ -48,7 +52,10 @@ export default function SignUp(){
         });
         const data = await response
         if(data.status === 200){
-            navigate('/login')
+            setIsLoading(false);
+            navigate('/login');
+        } else{
+            setIsLoading(false);
         }
     };
 
@@ -56,6 +63,25 @@ export default function SignUp(){
         <div className="signUp--container">
             <div className="form-box">
                 <h1>Create account</h1>
+                {isLoading === true &&     
+                <Oval
+                    height={100}
+                    width={100}
+                    radius={9}
+                    color="rgb(218, 37, 218)"
+                    ariaLabel="oval-loading"
+                    wrapperStyle={{
+                        position: "fixed",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        zIndex: 9999,
+                        borderRadius: "15px"
+                    }}
+                    wrapperClassName="loader"
+                />
+                }
+
                 <form onSubmit={handleSubmit}>
                     <div className="input-group">
                         <div className="input-field">
